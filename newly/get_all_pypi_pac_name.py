de3db49b292035ff5e99a147aacd6b65517f3cd7
OSSguardian <gaobanxiannb@163.com>
2025-02-10 16:43:06 +0800

    Update get_all_pypi_pac_name.py

--- newly/get_all_pypi_pac_name.py
+++ newly/get_all_pypi_pac_name.py
@@ -7,7 +7,6 @@ import time
 import datetime
 now = datetime.datetime.now()
 print(now)
-# 获取当前年月日
 today = now.strftime("%d")
 today = int(today)
 def get_all_pypi_packages():
@@ -15,17 +14,14 @@ def get_all_pypi_packages():
     url = "https://pypi.org/simple/"
 
     try:
-        # Send a GET request to the PyPI simple index
+
         response = requests.get(url, timeout=60)  # Set a timeout to avoid hanging indefinitely
         response.raise_for_status()  # Raise an error for bad status codes (4xx, 5xx)
 
-        # Parse the HTML content using BeautifulSoup
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        # Find all <a> tags, which contain the package names
         package_links = soup.find_all('a')
 
-        # Extract the package names from the href attributes and remove the 'simple/' prefix
         packages = [link.get('href').strip('/').replace('simple/', '') for link in package_links]
 
         return packages
@@ -48,10 +44,8 @@ def write_packages_to_csv(packages, filename=f'pypi_{today}.csv'):
 if __name__ == "__main__":
     start_time = time.time()
 
-    # Get the list of all PyPI packages
     packages = get_all_pypi_packages()
 
-    # Write the package names to a CSV file without the 'simple/' prefix and without a header row
     write_packages_to_csv(packages)
 
     end_time = time.time()
